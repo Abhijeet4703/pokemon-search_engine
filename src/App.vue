d0d8971455a2941453a11f62cd4904bca0ffbe85
Abhijeet4703 <144046903+Abhijeet4703@users.noreply.github.com>
2024-06-08 17:04:45 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="center-container">
+    <h1  class="img" style="text-align: center;">Pokémon Search</h1>
+    <div class="search-container">
+      <input type="text" v-model="searchTerm" @input="searchPokemon" placeholder="Enter Pokemon Name" class="search-input"/>
+      <button @click="searchPokemon">Submit</button>
+      <button @click="resetPokemon">Reset</button>
+      <div v-if="pokemon" class="pokemon-info">
+        <h2>Name: {{ pokemon.name }}</h2>
+        <img :src="pokemon.imageUrl" :alt="pokemon.name" class="pokemon-image" />
+        <h3>Abilities:</h3>
+        <ul>
+          <li v-for="ability in pokemon.abilities" :key="ability.ability.name">{{ ability.ability.name }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      searchTerm: '',
+      pokemon: null
+    }
+  },
+  methods: {
+    async searchPokemon () {
+      if (this.searchTerm === '') {
+        this.pokemon = null
+        return
+      }
+
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.searchTerm.toLowerCase()}`)
+        const data = await response.json()
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
+        data.imageUrl = imageUrl
+        this.pokemon = data
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        this.pokemon = null
+      }
+    },
+    resetPokemon () {
+      this.searchTerm = '' // Clear the search term
+      this.pokemon = null
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.center-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-image: url('./assets/p11.jpg');
+}
+
+.search-container {
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  background-color: #fff;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './assets/p11.jpg'
 
 createApp(App).use(store).use(router).mount('#app')
